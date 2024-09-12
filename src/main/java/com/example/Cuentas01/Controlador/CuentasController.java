e980cdb7e52641572735ff7526b0df883e856632
Ingrid <1415ingriddominguez@gmail.com>
2024-09-12 16:19:55 +0200

    Actualización

--- src/main/java/com/example/Cuentas01/Controlador/CuentasController.java
+++ src/main/java/com/example/Cuentas01/Controlador/CuentasController.java
@@ -6,20 +6,20 @@ import com.example.Cuentas01.CuentasService.CuentasService;
 import com.example.Cuentas01.Modelo.Gasto;
 
 @RestController
-@RequestMapping("/cuentas")
+@RequestMapping("/cuentas/gastos")
 public class CuentasController {
-
+	@CrossOrigin(origins = "http://localhost:4200")
 	//Endpoint para mostrar todos los gastos
-	@GetMapping(value="/gastos")
+	@GetMapping("/verTodo")
 	public ArrayList<Gasto> listaGastos(){
 		return CuentasService.listaGastos();
 	}
 	
 	//Método para buscar un gasto en concreto
-//	@GetMapping("/gastos/{name}")
-//	public Gasto findByName(@PathVariable("name") String name) {
-//		return CuentasService.buscarPorNombre(name);
-//	}
+	@GetMapping("/buscar")
+	public Gasto findByName(@RequestParam String nombre) {
+		return CuentasService.buscarPorNombre(nombre);
+	}
 	
 	//Endpoint para añadir un nuevo gasto
 	@PostMapping("/añadir")
@@ -28,8 +28,38 @@ public class CuentasController {
 	}
 	
 	//Endpoint para modificar un gasto
-	@PutMapping("/gastos/{nombre}")
-	public static void modificarGasto(@PathVariable("nombre") String nombre, @RequestBody Gasto gastoModificado) {
-		CuentasService.modificarGasto(nombre, gastoModificado);
+	@PutMapping("/modificar")
+	public static Gasto modificarGasto(@RequestParam String nombre, @RequestBody Gasto gastoModificado) {
+		return CuentasService.modificarGasto(nombre, gastoModificado);
+	}
+	
+	//Endpoint para eliminar un gasto
+	@DeleteMapping("/eliminar")
+	public static void eliminarGasto(@RequestParam String nombre) {
+		CuentasService.eliminarGasto(nombre);
+	}
+	
+	//Enpoint para mostrar el dinero restante
+	@GetMapping("/dineroRestante")
+	public static double dineroRestante(@RequestParam double sueldo) {
+		return CuentasService.dineroRestante(sueldo);
+	}
+	
+	//Enpoint para mostrar la suma de los gastos
+	@GetMapping("/sumaGastos")
+	public static double sumaGastos() {
+		return CuentasService.sumaGastos();
+	}
+	
+	//Enpoint para mostrar el 30% de margen (caprichos)
+	@GetMapping("/margen")
+	public static double dineroCaprichos(double sueldo) {
+		return CuentasService.dineroCaprichos(sueldo);
+	}
+	
+	//Enpoint para mostrar el total de ahorro
+	@GetMapping("/ahorro")
+	public static double ahorro(double sueldo) {
+		return CuentasService.ahorro(sueldo);
 	}
 }
--- src/main/java/com/example/Cuentas01/CuentasService/CuentasService.java
+++ src/main/java/com/example/Cuentas01/CuentasService/CuentasService.java
@@ -1,28 +1,28 @@
 package com.example.Cuentas01.CuentasService;
 
 import java.util.ArrayList;
-
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-
 import com.example.Cuentas01.Modelo.Gasto;
 
 @Service
 public class CuentasService {
 	//Creamos la variable con los gastos predefinidos
-		private static final ArrayList<Gasto> gastos = new ArrayList<Gasto>() {{
+	private static final ArrayList<Gasto> gastos = new ArrayList<Gasto>() {{
+	
+		add(new Gasto(24, "Internet"));
+		add(new Gasto(334, "Coche"));
+		add(new Gasto(461, "Alquiler"));
+		add(new Gasto(45, "Gym"));
+		add(new Gasto(3, "Spotify"));
+		add(new Gasto(10, "Anillo"));
+		add(new Gasto(50, "Gasolina"));
+		add(new Gasto(200, "Comida"));
+		add(new Gasto(6, "Netflix"));
 		
-			add(new Gasto(24, "internet"));
-			add(new Gasto(334, "coche"));
-			add(new Gasto(461, "alquiler"));
-			add(new Gasto(45, "gym"));
-			add(new Gasto(3, "Spotify"));
-			add(new Gasto(10, "anillo"));
-			add(new Gasto(50, "gasolina"));
-			add(new Gasto(200, "comida"));
-			add(new Gasto(6, "Netflix"));
-			
 	}};
+	
+	//Creamos la variable para guardar los resultados
+	
 
 	//Método para listar todos los gastos
 	public static ArrayList<Gasto> listaGastos(){
@@ -30,9 +30,9 @@ public class CuentasService {
 	}
 	
 	//Método para buscar un gasto en concreto
-	//public static Gasto buscarPorNombre(@PathVariable("name") String name) {
-		//return gastos.stream().filter(gastos -> gastos.getdescripcion().equals(name)).findFirst().orElse(null);
-	//}
+	public static Gasto buscarPorNombre(String name) {
+		return gastos.stream().filter(gastos -> gastos.getdescripcion().equalsIgnoreCase(name)).findFirst().orElse(null);
+	}
 	
 	//Método para añadir un nuevo gasto
 	public static Gasto añadirGasto(Gasto nuevoGasto) {
@@ -42,12 +42,49 @@ public class CuentasService {
 	}
 
 	//Método para modificar un gasto
-	public static void modificarGasto(String nombre, Gasto gastoModificado) {
-		for(Gasto gasto : gastos) {
-			if(gasto.getdescripcion().equalsIgnoreCase(nombre)) {
-				gasto.setCantidad(gastoModificado.getCantidad());
-	}
+	public static Gasto modificarGasto(String nombre, Gasto gastoModificado) {
+		Gasto gastoEncontrado = buscarPorNombre(nombre);
+		if(gastoEncontrado != null) {
+			gastoEncontrado.setCantidad(gastoModificado.getCantidad());
 		}
+		return gastoEncontrado;
+	}
+
+	//Método para eliminar un gasto
+	public static void eliminarGasto(String nombre) {
+		Gasto gastoEncontrado = buscarPorNombre(nombre);
+			if (gastoEncontrado != null) {
+				gastos.remove(gastoEncontrado);
+			}
+		}
+	
+	//Método para restar obtener el dinero restante
+	public static double dineroRestante(double sueldo) {
+		
+		return sueldo - sumaGastos();
+	}
+
+	//Método para sumar gastos
+	public static double sumaGastos(){
+		double totalGastos = 0;
+		
+		for(Gasto gasto : gastos) {
+		    totalGastos += gasto.getCantidad();
+		}
+		
+		return totalGastos;
 	}
 	
-}
+	//Método para calcular el 30% de margen (caprichos)
+	public static double dineroCaprichos(double sueldo) {
+		return (dineroRestante(sueldo) * 30) / 100;
+		
+	}
+		
+	//Método para calcular el total de ahorro
+	public static double ahorro(double sueldo) {
+		return dineroRestante(sueldo) - dineroCaprichos(sueldo);
+		
+	}
+	}
+
